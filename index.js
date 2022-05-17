e351db24f80459a6162fa81553fa1c14279e4d7b
Anna Grasmück <grasmueck.anna@gmail.com>
2022-05-17 14:00:49 +0200

    Add js-bookmark

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const button = document.querySelector('[data-js="button-answer"]');
 const answer = document.querySelector('[data-js="text-answer"]');
+const bookmark = document.querySelector('[data-js="js-bookmark"]')
 
         function onButtonClick (){
             answer.classList.toggle('card__answer--display')
         }
 
         button.addEventListener("click", onButtonClick)
+
+        function onBookmark (){
+            bookmark.classList.add('card__bookmark-icon--active')
+        }
+        bookmark.addEventListener("click", onBookmark)
