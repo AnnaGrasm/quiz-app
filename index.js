a8f18ba8f71937a10cc52104794cb420ff8be9c5
Anna Grasmück <grasmueck.anna@gmail.com>
2022-05-18 20:30:26 +0200

    Add js

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const buttonShow = document.querySelector('[data-js="button-show"]');
 const buttonHide = document.querySelector('[data-js="button-hide"]');
 const answer = document.querySelector('[data-js="text-answer"]');
 
+const bookmark =document.querySelector('[data-js="js-bookmark"]');
+
+
+const homeIcon = document.querySelector('[data-js="nav__icon-home"]');
+const homeSite = document.querySelector('[data-js="home-js"]');
+
+const bookmarkIcon = document.querySelector('[data-js="nav__icon-bookmark"]');
+const bookmarkSite = document.querySelector('[data-js="bookmark-js"]');
+
+const createIcon = document.querySelector('[data-js="nav__icon-create"]');
+const createSite = document.querySelector('[data-js="create-js"]');
+
+const profileIcon = document.querySelector('[data-js="nav__icon-profile"]');
+const profileSite = document.querySelector('[data-js="profile-js"]');
+
 
 
        
@@ -16,5 +31,15 @@ const answer = document.querySelector('[data-js="text-answer"]');
             buttonShow.classList.remove('card-display--none') 
         })
 
+       bookmark.addEventListener('click', () => {
+           bookmark.classList.toggle('card__bookmark-icon--active')
+       })
        
-       
+       homeIcon.addEventListener('click', () =>{
+        homeSite.classList.toggle("home")
+       })
+
+       bookmarkIcon.addEventListener('click', () =>{
+        bookmarkSite.classList.add("bookmark")
+       })
+
