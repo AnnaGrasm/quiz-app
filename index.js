338dcfd570534a20d2cf287693e2b5207c1b46e8
Anna Grasmück <grasmueck.anna@gmail.com>
2022-05-19 14:13:37 +0200

    Add lint

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const buttonShow = document.querySelector('[data-js="button-show"]');
 const buttonHide = document.querySelector('[data-js="button-hide"]');
 const answer = document.querySelector('[data-js="text-answer"]');
 
-const bookmark =document.querySelector('[data-js="js-bookmark"]');
-
+const bookmark = document.querySelector('[data-js="js-bookmark"]');
 
 const homeIcon = document.querySelector('[data-js="nav__icon-home"]');
 const homeSite = document.querySelector('[data-js="home-js"]');
@@ -17,60 +16,45 @@ const createSite = document.querySelector('[data-js="create-js"]');
 const profileIcon = document.querySelector('[data-js="nav__icon-profile"]');
 const profileSite = document.querySelector('[data-js="profile-js"]');
 
+buttonShow.addEventListener('click', () => {
+  answer.classList.add('card__answer--display');
+  buttonShow.classList.add('card-display--none');
+});
 
+buttonHide.addEventListener('click', () => {
+  answer.classList.remove('card__answer--display');
+  buttonShow.classList.remove('card-display--none');
+});
 
-       
+bookmark.addEventListener('click', () => {
+  bookmark.classList.toggle('card__bookmark-icon--active');
+});
 
-        buttonShow.addEventListener("click", () =>{
-            answer.classList.add('card__answer--display');
-            buttonShow.classList.add('card-display--none');
+//sections
+homeIcon.addEventListener('click', () => {
+  bookmarkSite.classList.remove('section--active');
+  createSite.classList.remove('section--active');
+  profileSite.classList.remove('section--active');
+  homeSite.classList.add('section--active');
+});
 
-        })
+bookmarkIcon.addEventListener('click', () => {
+  homeSite.classList.remove('section--active');
+  createSite.classList.remove('section--active');
+  profileSite.classList.remove('section--active');
+  bookmarkSite.classList.add('section--active');
+});
 
-        buttonHide.addEventListener("click", () => {
-            answer.classList.remove('card__answer--display');
-            buttonShow.classList.remove('card-display--none') 
-        })
+createIcon.addEventListener('click', () => {
+  homeSite.classList.remove('section--active');
+  bookmarkSite.classList.remove('section--active');
+  profileSite.classList.remove('section--active');
+  createSite.classList.add('section--active');
+});
 
-
-       
-       bookmark.addEventListener('click', () => {
-           bookmark.classList.toggle('card__bookmark-icon--active')
-       })
-       
-       //sections
-       homeIcon.addEventListener('click', () =>{
-        bookmarkSite.classList.remove("section--active");
-        createSite.classList.remove("section--active");
-        profileSite.classList.remove("section--active");
-        homeSite.classList.add("section--active");
-        
-        
-        
-
-       })
-
-       bookmarkIcon.addEventListener('click', () =>{
-        homeSite.classList.remove("section--active");
-        createSite.classList.remove("section--active");
-        profileSite.classList.remove("section--active");
-        bookmarkSite.classList.add("section--active");
-
-       })
-
-       createIcon.addEventListener('click', () => {
-        homeSite.classList.remove("section--active");
-        bookmarkSite.classList.remove("section--active");
-        profileSite.classList.remove("section--active");
-        createSite.classList.add("section--active");
-       })
-
-       profileIcon.addEventListener('click', () => {
-        homeSite.classList.remove("section--active");
-        bookmarkSite.classList.remove("section--active");
-        createSite.classList.remove("section--active");
-        profileSite.classList.add("section--active");
-       })
-
-
-
+profileIcon.addEventListener('click', () => {
+  homeSite.classList.remove('section--active');
+  bookmarkSite.classList.remove('section--active');
+  createSite.classList.remove('section--active');
+  profileSite.classList.add('section--active');
+});
