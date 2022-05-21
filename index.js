2f1a477b4d2a2ba8f6f2c5091ad3a4bcc6ee3693
Anna Grasmück <grasmueck.anna@gmail.com>
2022-05-21 19:32:32 +0200

    Add Elements

--- index.js
+++ index.js
@@ -1,62 +1,7 @@
 import { bookmarks } from './js/bookmarks.js';
 import { answerButton } from './js/answerButton.js';
+import { navigation } from './js/navigation.js';
 
 bookmarks();
 answerButton();
-
-const homeIcon = document.querySelector('[data-js="nav__icon-home"]');
-const homeSite = document.querySelector('[data-js="home-js"]');
-
-const bookmarkIcon = document.querySelector('[data-js="nav__icon-bookmark"]');
-const bookmarkSite = document.querySelector('[data-js="bookmark-js"]');
-
-const createIcon = document.querySelector('[data-js="nav__icon-create"]');
-const createSite = document.querySelector('[data-js="create-js"]');
-
-const profileIcon = document.querySelector('[data-js="nav__icon-profile"]');
-const profileSite = document.querySelector('[data-js="profile-js"]');
-
-//sections
-homeIcon.addEventListener('click', () => {
-  bookmarkSite.classList.remove('section--active');
-  createSite.classList.remove('section--active');
-  profileSite.classList.remove('section--active');
-  homeSite.classList.add('section--active');
-});
-
-bookmarkIcon.addEventListener('click', () => {
-  homeSite.classList.remove('section--active');
-  createSite.classList.remove('section--active');
-  profileSite.classList.remove('section--active');
-  bookmarkSite.classList.add('section--active');
-});
-
-createIcon.addEventListener('click', () => {
-  homeSite.classList.remove('section--active');
-  bookmarkSite.classList.remove('section--active');
-  profileSite.classList.remove('section--active');
-  createSite.classList.add('section--active');
-});
-
-profileIcon.addEventListener('click', () => {
-  homeSite.classList.remove('section--active');
-  bookmarkSite.classList.remove('section--active');
-  createSite.classList.remove('section--active');
-  profileSite.classList.add('section--active');
-});
-
-/* Exersice */
-
-function button() {
-  const AllfirstButton = document.querySelectorAll('.first');
-
-  const text = document.querySelectorAll('.first-text');
-
-  AllfirstButton.forEach((firstButton, index) => {
-    firstButton.addEventListener('click', () => {
-      firstButton.classList.toggle('first--change');
-      text[index].classList.toggle('first-text--none');
-    });
-  });
-}
-button();
+navigation();
---
+++ js/navigation.js
@@ -0,0 +1,42 @@
+export function navigation() {
+  const homeIcon = document.querySelector('[data-js="nav__icon-home"]');
+  const homeSite = document.querySelector('[data-js="home-js"]');
+
+  const bookmarkIcon = document.querySelector('[data-js="nav__icon-bookmark"]');
+  const bookmarkSite = document.querySelector('[data-js="bookmark-js"]');
+
+  const createIcon = document.querySelector('[data-js="nav__icon-create"]');
+  const createSite = document.querySelector('[data-js="create-js"]');
+
+  const profileIcon = document.querySelector('[data-js="nav__icon-profile"]');
+  const profileSite = document.querySelector('[data-js="profile-js"]');
+
+  homeIcon.addEventListener('click', () => {
+    homeSite.classList.remove('section--active');
+    bookmarkSite.classList.remove('section--active');
+    createSite.classList.remove('section--active');
+    profileSite.classList.remove('section--active');
+    homeSite.classList.add('section--active');
+  });
+
+  bookmarkIcon.addEventListener('click', () => {
+    homeSite.classList.remove('section--active');
+    createSite.classList.remove('section--active');
+    profileSite.classList.remove('section--active');
+    bookmarkSite.classList.add('section--active');
+  });
+
+  createIcon.addEventListener('click', () => {
+    homeSite.classList.remove('section--active');
+    bookmarkSite.classList.remove('section--active');
+    profileSite.classList.remove('section--active');
+    createSite.classList.add('section--active');
+  });
+
+  profileIcon.addEventListener('click', () => {
+    homeSite.classList.remove('section--active');
+    bookmarkSite.classList.remove('section--active');
+    createSite.classList.remove('section--active');
+    profileSite.classList.add('section--active');
+  });
+}
